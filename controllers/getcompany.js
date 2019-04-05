7bca41ef5e9f519590e76353a2961ec39afdd8ca
krschnoor <krschnoor63@gmail.com>
2019-04-05 10:01:21 -0400

    none

--- controllers/getcompany.js
+++ controllers/getcompany.js
@@ -31,9 +31,9 @@ exports.getCompany = function (req, res) {
         console.log(qbo)
 
 
-        qbo.getCompanyInfo(session.id, function (_, res) {
+        qbo.getCompanyInfo(session.id, function (_, response) {
 
-            console.log(res)
+            console.log(response)
 
             res.status(200).json({})
 
@@ -46,23 +46,65 @@ exports.getCompany = function (req, res) {
 function getRefreshToken(req, res, callback) {
 
     var auth = (new Buffer(consumerKey + ':' + consumerSecret).toString('base64'));
+    var realmid = session.id
+    //need to add client realmid and pass to function to get correct refresh token. realm id should be passed in as a query parameter
+
+    try {
+        refreshToken.getRefreshToken(res, realmid, function (token) {
+
+            console.log(token)
+            if (token == -1) {
+                res.send("<InputTable></InputTable>")
+            }
+
+            var postBody = {
+                url: QuickBooks.TOKEN_URL,
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Authorization: 'Basic ' + auth,
+                },
+                form: {
+                    grant_type: 'refresh_token',
+                    refresh_token: token.refreshtoken //qbo.refreshToken 
+                }
+            };
 
 
+            request.post(postBody, (function (e, r, data) {
+                console.log('here is r' + r)
+                if (r && r.body) {
+                    var refreshResponse = JSON.parse(r.body);
+                    // this.refreshToken = refreshResponse.refresh_token;
+                    //this.token = refreshResponse.access_token;
 
-    qbo = new QuickBooks(consumerKey,
-        consumerSecret,
-        session.accessToken, /* oAuth access token */
-        false, /* no token secret for oAuth 2.0 */
-        session.id,
-        false, /* use a sandbox account */
-        true, /* turn debugging on */
-        4, /* minor version */
-        '2.0', /* oauth version */
-        session.refreshToken /* refresh token */)
+                    console.log('refreshtoken' + refreshResponse.refresh_token)
+                    console.log('accesstoken' + refreshResponse.access_token)
+                    console.log('refreshresponse' + refreshResponse)
+
+                    qbo = new QuickBooks(consumerKey,
+                        consumerSecret,
+                        refreshResponse.access_token, /* oAuth access token */
+                        false, /* no token secret for oAuth 2.0 */
+                        token.realmid,
+                        false, /* use a sandbox account */
+                        true, /* turn debugging on */
+                        4, /* minor version */
+                        '2.0', /* oauth version */
+                        refreshResponse.refresh_token /* refresh token */);
 
 
+                    refreshToken.storeRefreshToken(refreshResponse.refresh_token, token.realmid) // store new refresh token in mongo
 
-    callback()
 
+                    callback()
 
+                }
+
+            }).bind(this));
+        })
+    } catch (e) {
+
+        res.send("<InputTable></InputTable>")
+    }
 }
