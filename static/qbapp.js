676c0f825c2fd086462f5e2d4288d274b89b5835
krschnoor <krschnoor63@gmail.com>
2019-04-08 15:56:26 -0400

    none

--- static/qbapp.js
+++ static/qbapp.js
@@ -10,7 +10,7 @@ app.controller('QBOLcontroller', ['$scope', '$http', '$location', '$timeout', '$
 
   $scope.getQB = function () {
 
-    alert("hello")
+   
     var x = $window.open('/qb') //change to localhost
 
     $timeout(function () {
@@ -20,17 +20,7 @@ app.controller('QBOLcontroller', ['$scope', '$http', '$location', '$timeout', '$
 
   }
 
-  $scope.getAccounts = function (realmid) {
-
-    alert("getaccounts")
-    $http.get('/getcompany', { params: { realmid: realmid } }).success(function (data, status, headers, config) {
-
-      alert(data.id)
-      // $scope.QBAccounts = data
-      // $scope.content = '/static/' + 'hello.html'
-    }).error(function (data, status, headers, config) { alert(data) })
-
-  }
+  
 
 
   $scope.testKeys = function () {
@@ -54,54 +44,11 @@ app.controller('QBOLcontroller', ['$scope', '$http', '$location', '$timeout', '$
     $scope.content = '/static/' + page
   }
 
-
-  $scope.getJson = function () {
-
-    $http.post('/getjson/', { message: "This is a Json Object" })
-      .success(function (data, status, headers, config) {
-        alert(data)
-      })
-  }
+ 
 
 
 
-
-  $scope.getTB = function (realmid) {
-
-    alert(realmid)
-
-    $http.get('/tb', { params: { realmid: realmid, dtStart: "2019-1-1", dtEnd: "2019-3-31" } }).success(function (data, status, headers, config) {
-
-      console.log(data)
-      // $scope.tb = data
-      // $scope.setContent('trialbalanceview.html')
-      // $scope.content = '/static/' + 'trialbalanceview.html'
-    }).error(function (data, status, headers, config) { alert(data) })
-
-  }
-
-  $scope.getTrialBalanceViewTotals = function (type) {
-
-
-    var totals = {
-
-      debits: 0,
-      credits: 0
-    }
-
-    $scope.tb.forEach(function (account, i) {
-
-
-      totals.debits += parseFloat(account.debit) || 0
-      totals.credits += parseFloat(account.credit) || 0
-
-
-    })
-
-
-    return type == "credit" ? totals.credits : totals.debits
-  }
-
+ 
 
   $scope.copy = function () {
    
